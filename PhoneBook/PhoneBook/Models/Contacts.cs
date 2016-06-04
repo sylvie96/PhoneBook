b4e770b923bc61ab554b896dc388c5d621fb2b2b
sylvie96 <sisigarcheva@abv.bg>
2016-06-04 11:27:28 +0300

    second

---
+++ PhoneBook/PhoneBook/Models/Contacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Models
+{
+    public class Contacts
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Num { get; set; }
+        public string Adress { get; set; }
+    }
+    public class ContactsDBContext :DbContext
+    {
+        public DbSet<Contacts> Contacts { get; set; }
+    }
+}
